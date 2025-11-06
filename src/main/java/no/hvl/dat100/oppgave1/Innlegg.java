2abf0313cc872b4bf84b797670926bf27c474929
simulert <gunnargustad@hotmail.com>
2025-11-06 15:55:24 +0100

    1

--- src/main/java/no/hvl/dat100/oppgave1/Innlegg.java
+++ src/main/java/no/hvl/dat100/oppgave1/Innlegg.java
@@ -5,6 +5,10 @@ import no.hvl.dat100.common.TODO;
 public abstract class Innlegg {
 	
 	// TODO - deklarering av objektvariable
+	private int id;
+	private String bruker;
+	private String dato;
+	private int likes;
 	
 	public Innlegg() {
 		
@@ -13,57 +17,73 @@ public abstract class Innlegg {
 	public Innlegg(int id, String bruker, String dato) {
 
 		// TODO 
+		this.id = id;
+		this.bruker = bruker;
+		this.dato = dato;
+		this.likes = 0;
+		
 		throw new UnsupportedOperationException(TODO.constructor("Innlegg"));
 	}
 
 	public Innlegg(int id, String bruker, String dato, int likes) {
 
 		// TODO - START
+		this.id = id;
+		this.bruker = bruker;
+		this.dato = dato;
+		this.likes = likes;
 		
 		throw new UnsupportedOperationException(TODO.constructor("Innlegg"));
 	}
 	
 	public String getBruker() {
-		
+		return bruker;
 		throw new UnsupportedOperationException(TODO.method());
 
 	}
 
 	public void setBruker(String bruker) {
+		this.bruker = bruker;
 		throw new UnsupportedOperationException(TODO.method());
 	}
 
 	public String getDato() {
+		return dato;
 		throw new UnsupportedOperationException(TODO.method());
 		
 	}
 
 	public void setDato(String dato) {
+		this.dato = dato;
 		throw new UnsupportedOperationException(TODO.method());
 	}
 
 	public int getId() {
+		return id;
 		throw new UnsupportedOperationException(TODO.method());
 
 	}
 
 	public int getLikes() {
+		return likes;
 		throw new UnsupportedOperationException(TODO.method());
 
 	}
 	
 	public void doLike () {
+		this.likes += 1;
 		throw new UnsupportedOperationException(TODO.method());
 	}
 	
 	public boolean erLik(Innlegg innlegg) {
+		return this.id == innlegg.getId();
 		throw new UnsupportedOperationException(TODO.method());
 
 	}
 	
 	@Override
 	public String toString() {
-		
+		return this.id +"\n" + this.bruker + "\n" + this.dato + "\n" + this.likes + "\n";
 		throw new UnsupportedOperationException(TODO.method());
 				
 	}
