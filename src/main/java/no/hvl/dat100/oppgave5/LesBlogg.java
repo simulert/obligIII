984cf934128789236d0feb07c24aaa7bbe8e6b6c
simulert <gunnargustad@hotmail.com>
2025-11-06 19:08:35 +0100

    5

--- src/main/java/no/hvl/dat100/oppgave5/LesBlogg.java
+++ src/main/java/no/hvl/dat100/oppgave5/LesBlogg.java
@@ -21,8 +21,44 @@ public class LesBlogg {
 	private static String BILDE = "BILDE";
 
 	public static Blogg les(String mappe, String filnavn) {
-
+		
 		throw new UnsupportedOperationException(TODO.method());
+		Blogg blogg = null;
+        String filepath = mappe + "/" + filnavn;
 
+        try (Scanner leser = new Scanner(new File(filepath))) {
+
+            int antall = Integer.parseInt(leser.nextLine());
+            blogg = new Blogg(antall);
+
+            for (int i = 0; i < antall; i++) {
+                String type = leser.nextLine();
+
+                int id = Integer.parseInt(leser.nextLine());
+                String bruker = leser.nextLine();
+                String dato = leser.nextLine();
+                int likes = Integer.parseInt(leser.nextLine());
+
+                if (type.equals(TEKST)) {
+                    String tekst = leser.nextLine();
+                    Tekst innlegg = new Tekst(id, bruker, dato, likes, tekst);
+                    blogg.leggTil(innlegg);
+                } 
+                else if (type.equals(BILDE)) {
+                    String tekst = leser.nextLine();
+                    String url = leser.nextLine();
+                    Bilde innlegg = new Bilde(id, bruker, dato, likes, tekst, url);
+                    blogg.leggTil(innlegg);
+                }
+            }
+
+        } catch (FileNotFoundException e) {
+            System.out.println("Filen ble ikke funnet: " + filepath);
+        } catch (Exception e) {
+            System.out.println("Noe gikk galt ved lesing: " + e.getMessage());
+        }
+
+        return blogg;
+		
 	}
 }
