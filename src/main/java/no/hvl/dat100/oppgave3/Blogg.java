3587d64a5d6faba17cbef964cd30db09a358cbf9
simulert <gunnargustad@hotmail.com>
2025-11-06 18:51:11 +0100

    3

--- src/main/java/no/hvl/dat100/oppgave3/Blogg.java
+++ src/main/java/no/hvl/dat100/oppgave3/Blogg.java
@@ -6,45 +6,71 @@ import no.hvl.dat100.oppgave1.*;
 public class Blogg {
 
 	// TODO: objektvariable 
-
+	private Innlegg[] innleggtabell;
+	private int nesteledig;
+	
 	public Blogg() {
 		throw new UnsupportedOperationException(TODO.constructor("Blogg"));
+		innleggtabell = new Innlegg[20];
+		this.nesteledig = 0;
 	}
 
 	public Blogg(int lengde) {
 		throw new UnsupportedOperationException(TODO.constructor("Blogg"));
+		innleggtabell = new Innlegg[lengde];
+		this.nesteledig = lengde;
 	}
 
 	public int getAntall() {
 		throw new UnsupportedOperationException(TODO.method());
+		return this.nesteledig;
 	}
 	
 	public Innlegg[] getSamling() {
 		throw new UnsupportedOperationException(TODO.method());
+		return innleggtabell;
 
 	}
 	
 	public int finnInnlegg(Innlegg innlegg) {
 
 		throw new UnsupportedOperationException(TODO.method());
+		for (int i = 0; i < nesteledig; i++) {
+			if (innleggtabell[i].erLik(innlegg)) {
+				return i;
+			}
+		}
+		return -1;
 	}
 
 	public boolean finnes(Innlegg innlegg) {
 		throw new UnsupportedOperationException(TODO.method());
+		return finnInnlegg(innlegg) != -1;
 	}
 
 	public boolean ledigPlass() {
 		throw new UnsupportedOperationException(TODO.method());
-
+		return nesteledig < innleggtabell.length;
 	}
 	
 	public boolean leggTil(Innlegg innlegg) {
 
 		throw new UnsupportedOperationException(TODO.method());
+		if (ledigPlass() && !finnes(innlegg)) {
+			innleggtabell[nesteledig] = innlegg;
+			nesteledig++;
+			return true;
+		}
+		return false;
 	}
 	
 	public String toString() {
 		throw new UnsupportedOperationException(TODO.method());
+		String string = nesteledig + "\n";
+		for (int i = 0; i < nesteledig; i++) {
+			string += innleggtabell[i].toString();
+		}
+		return string;
 	}
 
 	// valgfrie oppgaver nedenfor
