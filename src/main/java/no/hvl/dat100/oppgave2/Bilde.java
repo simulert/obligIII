21488f113bb30f35c965a0210509f2582f4722ea
simulert <gunnargustad@hotmail.com>
2025-11-06 18:26:40 +0100

    2

--- src/main/java/no/hvl/dat100/oppgave2/Bilde.java
+++ src/main/java/no/hvl/dat100/oppgave2/Bilde.java
@@ -5,8 +5,11 @@ import no.hvl.dat100.common.TODO;
 public class Bilde extends Tekst {
 
 	// TODO - objekt variable
+	private String url;
 	
 	public Bilde(int id, String bruker, String dato, String tekst, String url) {
+		super(id, bruker, dato, tekst);
+		this.url = url;
 		throw new UnsupportedOperationException(TODO.constructor("Bilde"));
 	}
 
@@ -15,16 +18,19 @@ public class Bilde extends Tekst {
 	}
 	
 	public String getUrl() {
+		return this.url;
 		throw new UnsupportedOperationException(TODO.method());
 
 	}
 
 	public void setUrl(String url) {
+		this.url = url;
 		throw new UnsupportedOperationException(TODO.method());
 	}
 
 	@Override
 	public String toString() {
+		return "BILDE\n"+ this.id + "\n" + this.bruker + "\n" + this.dato. + "\n" + this.likes + "\n" + this.url + "\nhttp://www.picture.com/oo.jpg\n";
 		throw new UnsupportedOperationException(TODO.method ());
 
 	}
--- src/main/java/no/hvl/dat100/oppgave2/Tekst.java
+++ src/main/java/no/hvl/dat100/oppgave2/Tekst.java
@@ -5,31 +5,39 @@ import no.hvl.dat100.oppgave1.*;
 
 public class Tekst extends Innlegg {
 
-	// TODO: objektvariable 
+	// TODO: objektvariable
+	private String tekst;
 	
 	public Tekst () {
 		
 	}
 	
 	public Tekst(int id, String bruker, String dato, String tekst) {
+		super(id, bruker, dato);
+		this.tekst = tekst;
 		throw new UnsupportedOperationException(TODO.constructor("Tekst"));
 	}
 	
 	public Tekst(int id, String bruker, String dato, int likes, String tekst) {
+		super(id, bruker, dato, likes);
+		this.tekst = tekst;
 		throw new UnsupportedOperationException(TODO.constructor("Tekst"));
 	}
 	
 	public String getTekst() {
+		return tekst;
 		throw new UnsupportedOperationException(TODO.method());
 
 	}
 
 	public void setTekst(String tekst) {
+		this.tekst = tekst;
 		throw new UnsupportedOperationException(TODO.method());
 	}
 
 	@Override
 	public String toString() {
+		return "TEKST\n" + this.id + "\n" + this.bruker + "\n" + this.dato + "\n" + this.likes + "\n" + this.tekst + "\n";
 		throw new UnsupportedOperationException(TODO.method());
 
 	}
